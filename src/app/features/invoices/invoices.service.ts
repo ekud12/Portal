9f020db97295404db7a88fe919b5f5da845f2b4b
liel.k <liel.k@meuhedet.co.il>
2018-04-10 15:39:36 +0300

    bad day changes 3

--- src/app/features/invoices/invoices.service.ts
+++ src/app/features/invoices/invoices.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { BackendService } from '../../core/services/backend.service';
-import { GetInvoicesRequest } from './models/new-actions.model';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import { SapakDataRequest } from '../user/models/sapak.model';
 
 @Injectable()
 export class InvoicesService {
   constructor(private backendService: BackendService) {}
 
-  getAllInvoicesForSapak(request: GetInvoicesRequest): Observable<any> {
+  getAllInvoicesForSapak(request: SapakDataRequest): Observable<any> {
     return Observable.of([
       { invoiceNum: 1, billMonth: '12/2018', totalRowsNum: 1, invoiceSum: 1579, status: 0 },
       { invoiceNum: 2, billMonth: '12/2018', totalRowsNum: 1, invoiceSum: 1579, status: 0 },
