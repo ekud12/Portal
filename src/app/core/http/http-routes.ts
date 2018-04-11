600777c44a7874cc96897e2f04ba8cf5d8fd0ae3
liel.k <liel.k@meuhedet.co.il>
2018-04-11 19:36:39 +0300

    11042018 c12 final commit +refractor

--- src/app/core/http/http-routes.ts
+++ src/app/core/http/http-routes.ts
@@ -1,14 +1,20 @@
 export enum httpRoutes {
   /** Login Endpoints */
   LOGIN = 'token',
+
   /** Zakaut Module Server Endpoints */
   ZAKAUT_API = 'api/zakaut',
+
   /** Invoices Module Server Endpoints */
   INVOICES_API = 'api/invoices',
   INVOICES_GET_ALL_INVOICES = 'api/invoices/GetAllInvoices',
-  /** Treatments Server Endpoints */
+  INVOICES_GET_ALL_ROWS = 'api/invoices/GetInvoiceRows',
+  INVOICES_GET_ALL_TREATMENTS = 'api/invoices/GetInvoiceTreatments',
+
+  /** Treatments FOR SAPAK Server Endpoints */
   TREATMENTS_API = 'api/treatments',
   TREATMENTS_FOR_SAPAK_EP = 'api/treatments/GetTreatmentsForSapak',
+
   /** FalconX Module Server Endpoints */
   FALCONX_API = 'api/falconx'
 }
